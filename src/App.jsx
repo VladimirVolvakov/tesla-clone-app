bb3baf28ed508dd2841f908e1604a647881326c8
VladimirVolvakov <dr.volvakov@gmail.com>
2022-08-12 00:32:45 +0300

    feat: add Section component template

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 // Components:
-import Header from './components/Header';
-import Home from './components/Home';
+import Header from './components/Header/Header';
+import Home from './components/Home/Home';
 // Styles:
 import './App.css';
 
---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,14 @@
+// Components:
+import Section from "../Section/Section";
+// Styles:
+import { Container } from "./Home.styles";
+
+const Home = () => {
+  return (
+    <Container>
+      <Section />
+    </Container>
+  );
+};
+
+export default Home;
