c891bc417dc3d7cde329ccf8f4ecd05bdd085ef9
VladimirVolvakov <dr.volvakov@gmail.com>
2022-08-11 23:29:06 +0300

    feat: add Header & Home components templates

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+// Components:
+import Header from './components/Header';
+import Home from './components/Home';
+// Styles:
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Home />
+    </div>
+  );
+}
+
+export default App;
