dfef279f5d2e9e407b99b59d5b176c2b13de807d
VladimirVolvakov <dr.volvakov@gmail.com>
2022-08-13 23:56:01 +0300

    feat: add Section component markup

--- src/components/Section/Section.jsx
+++ src/components/Section/Section.jsx
@@ -1,10 +1,22 @@
 // Styles:
-import { Wrapper } from "./Section.styles";
+import { ButtonGroup, LeftButton, RightButton, TextItem, Wrapper } from "./Section.styles";
 
 const Section = () => {
   return (
     <Wrapper>
-      Section
+      <TextItem>
+        <h1>Model S</h1>
+        <p>Order Online for Touchless Delivery</p>
+      </TextItem>
+
+      <ButtonGroup>
+        <LeftButton>
+          Custom Order
+        </LeftButton>
+        <RightButton>
+          Existing Inventory
+        </RightButton>
+      </ButtonGroup>
     </Wrapper>
   );
 };
--- src/components/Section/Section.styles.js
+++ src/components/Section/Section.styles.js
@@ -1,3 +1,11 @@
 import styled from "styled-components";
 
 export const Wrapper = styled.div``;
+
+export const TextItem = styled.div``;
+
+export const ButtonGroup = styled.div``;
+
+export const LeftButton = styled.div``; 
+
+export const RightButton = styled.div``;
